af4e332f3e441a2279bd291a7b28e6ba8e006a1d
Thiyunu <dinal.bandara@gmail.com>
2024-09-18 21:46:20 +0530

    feat: Connecting metamask using rainbowkit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
   return (
     <div className="flex w-full justify-center items-center p-20">
-      <p>Main Page</p>
+      <ConnectButton />
     </div>
   );
 }
